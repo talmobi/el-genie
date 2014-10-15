d75b8e7a082883870038a0723586ac4e4fa36e56
Hannyajin <talmo.christian@gmail.com>
2014-10-15 03:35:05 +0300

    hitbox visual debug relative

--- js/genie_app.js
+++ js/genie_app.js
@@ -600,8 +600,8 @@ var elGenie = (function() {
     // draw hitbox for testing
     var g = new PIXI.Graphics();
     g.beginFill(0x00FF00);
-    g.drawRect(sprGenie.hitArea.x,
-               sprGenie.hitArea.y, sprGenie.hitArea.width, sprGenie.hitArea.height);
+    g.drawRect(sprGenie.hitArea.x + sprGenie.width * sprGenie.anchor.x,
+               sprGenie.hitArea.y + sprGenie.height * sprGenie.anchor.y, sprGenie.hitArea.width, sprGenie.hitArea.height);
     g.endFill();
     stage.addChild(g);
 
